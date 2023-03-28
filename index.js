7c31fb5de3f7492b85d3a4f7fc4394594715e2a1
Nahin27 <nahin271199@gmail.com>
2023-03-28 13:41:41 +0600

    3.2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const numOfPeople = `<h1>Phonebook has info for ${persons.length} people</h1>`
+    const date = new Date()
+    const currentTime = `<h1>${date}</h1>`
+    response.send(`${numOfPeople}\n${currentTime}`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
